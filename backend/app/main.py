411e8865e6523c7f18d393e33ce8a273e59f6a1c
Kapil Mahale <kapilmahale2@gmail.com>
2026-07-03 02:05:04 +0530

    origin updated

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,7 +15,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://resume-genie-rust.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
